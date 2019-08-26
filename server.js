a81d78f50041468d2c6d98ef5f168d9c7ea2f4ba
rick <ricardo101289@hotmail.com>
2019-08-26 14:43:56 -0500

    enable coors

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ var app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
  
 var users = ['oscar', 'juan', 'marcos']
 
